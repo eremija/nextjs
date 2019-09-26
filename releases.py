b9662f510e2c278943e91eec210580845b5089df
Marko Eremija <eremija@work.co>
2019-09-26 12:41:11 +0200

    Blah

--- releases.py
+++ releases.py
@@ -2,9 +2,7 @@ import urllib.request, json, os
 
 with urllib.request.urlopen("https://api.github.com/repos/eremija/nextjs/releases") as url:
     data = json.loads(url.read().decode())
-    print(data[0]["tag_name"])
     git_tag = data[0]["tag_name"]
 
-print(git_tag)
-
 os.environ["CIRCLE_TAG"]=str(git_tag)
+print(os.environ["CIRCLE_TAG"])
